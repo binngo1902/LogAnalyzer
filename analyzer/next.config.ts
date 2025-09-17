f8e561da2516e5e34275d08c97b7fb1be25c8446
BinhNgo <binngo1902@gmail.com>
2025-09-18 00:27:38 +0700

    - update code

--- analyzer/next.config.ts
+++ analyzer/next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/LogAnalyzer",
-  assetPrefix: '/LogAnalyzer',
   images: {
     unoptimized: true,
   },
